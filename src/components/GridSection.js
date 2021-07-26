919f36cdfb1ab751a580e18999f21c5f582efb15
Stackbit Code Editor <projects@stackbit.com>
2021-07-26 09:57:36 +0000

    GridSection.js: changed

--- src/components/GridSection.js
+++ src/components/GridSection.js
@@ -27,7 +27,7 @@ export default class GridSection extends React.Component {
                             )}
                             {_.get(item, 'grid_title', null) && (
                             <div className="grid-item-title">
-                                <h4>{(_.get(item, 'grid_title', null))} </h4>
+                                <div className="">{(_.get(item, 'grid_title', null))} </h4>
                             </div>
                             )}
                         </div>
