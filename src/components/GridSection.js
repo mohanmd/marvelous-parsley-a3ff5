11e00d0334f1164433b90d00ae87367c50749e36
Stackbit Code Editor <projects@stackbit.com>
2021-07-26 05:35:31 +0000

    GridSection.js: changed

--- src/components/GridSection.js
+++ src/components/GridSection.js
@@ -1,4 +1,5 @@
 import React from 'react'; 
+import _ from 'lodash';
 import {classNames, htmlToReact, withPrefix, Link, markdownify} from '../utils';
 
 export default class GridSection extends React.Component {
