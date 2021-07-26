95573622c17433ff4a6cbb94b210e6b2e84331fa
Stackbit Code Editor <projects@stackbit.com>
2021-07-26 09:23:46 +0000

    GridSection.js: changed

--- src/components/GridSection.js
+++ src/components/GridSection.js
@@ -8,7 +8,7 @@ export default class GridSection extends React.Component {
         let section = _.get(this.props, 'section', null);
 
         return (
-            <section className={classNames('Gridsection',_.get(section, 'class_name', null))} style={toStyleObj('background-image: url(\'' + withPrefix(_.get(section, 'image', null)) + '\')')}>  
+            <section className={classNames('Gridsection',_.get(section, 'class_name', null))} style={toStyleObj('background-image: url(\'' + withPrefix(_.get(section, 'bg_image', null)) + '\')')}>  
                 <div className="container container--lg">
                     {_.get(section, 'section_title', null) && (
                     <h1 className="block-title">{_.get(section, 'section_title', null)}</h1>
