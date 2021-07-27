79c9a9740bebfc1e9d1396043f79a8c6ff0b3127
Stackbit Code Editor <projects@stackbit.com>
2021-07-27 05:49:38 +0000

    contact_section.js: changed

--- src/components/contact_section.js
+++ src/components/contact_section.js
@@ -3,8 +3,7 @@ import _ from 'lodash';
 import {classNames, htmlToReact, withPrefix, Link, markdownify, toStyleObj} from '../utils';
 
 export default class Contact extends React.Component {
-    render() {
-        console.log( 'contact:///' );
+    render() { 
         console.log(this.props );
         // let section = _.get(this.props, 'section', null);
 
